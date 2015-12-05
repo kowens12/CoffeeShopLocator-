c066fac57645770b964a73e9735266f0b59c48fb
Brian Glowe <brianglowe@gmail.com>
2015-12-05 17:57:24 -0500

    Added code and comments to respond to the venue data notification

--- Coffee/ViewController.swift
+++ Coffee/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("onVenuesUpdated:"), name: API.notifications.venuesUpdated, object: nil)
+        //^^ this will tell the notification center that self is listening to a notification of type API.notifications.venuesUpdated
         
         if let mapView = self.mapView {
             mapView.delegate = self
@@ -41,17 +43,28 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             locationManager!.startUpdatingLocation()
         }
     }
+    
+    func onVenuesUpdated(notification: NSNotification) {
+        refreshVenues(nil)
+    }
+    
+    
 /* we need to be sure we have configured the token to receive permission to use the user's location
 THIS is performed in the plist, first add the NSLocationAlwaysUsageDescription row and add a string to prompt the user. */
     
     //First, the method signature is locationManager:didUpdateToLocation:fromLocation. The method uses named parameters, which means that the name of the parameter (variable inside the method) is different from the argument used when calling the method. In short, the method has 3 parameters: the location manager that called the method, the new GPS location, and the old GPS location
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        
         //Inside the method, first self.mapView is unwrapped using optional binding. When self.mapView is not nil, mapView now contains the unwrapped non-optional value, and the if-statement is executed
         if let mapView = self.mapView {
+            
             //In the if-statement, a region is calculated from the new GPS coordinate and the distanceSpan we created earlier. Technically, it creates a rectangular region of 500 by 500 meters with the newLocation coordinate in the center
             let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceSpan, distanceSpan)
+            
             //Finally, it calls setRegion on the map view. The named parameter animated is set to true, so the region change is animated. In other words: the map will pan and zoom so it shows the map for the user’s location and 500×500 square meter around it
             mapView.setRegion(region, animated: true)
+            
+            refreshVenues(newLocation, getDataFromFoursquare: true)
         }
     }
     
